777fafe52a0cb167b3441810cc2eb746c294b4f7
Florian Braun <Braunf25@proton.me>
2025-02-10 14:43:58 +0100

    90% completion part 2 & 3

--- 01-oo/03-properties/assignments/06-time/student.py
+++ 01-oo/03-properties/assignments/06-time/student.py
@@ -1,1 +1,41 @@
-# Write your code here
+class Time:
+    def __init__(self, hours, minutes, seconds):
+        self.__hours = hours
+        self.__minutes = minutes
+        self.__seconds = seconds
+        if not 0 <= hours <= 23:
+            raise ValueError("Hours must be between 0 and 23")
+        if not 0 <= minutes <= 59:
+            raise ValueError("Minutes must be between 0 and 59")
+        if not 0 <= seconds <= 59:
+            raise ValueError("Seconds must be between 0 and 59")
+
+    @property
+    def hours(self):
+        return self.__hours
+
+    @hours.setter
+    def hours(self, value):
+        if not 0 <= value <= 23:
+            raise ValueError("Hours must be between 0 and 23")
+        self.__hours = value
+
+    @property
+    def minutes(self):
+        return self.__minutes
+
+    @minutes.setter
+    def minutes(self, value):
+        if not 0 <= value <= 59:
+            raise ValueError("Minutes must be between 0 and 59")
+        self.__minutes = value
+
+    @property
+    def seconds(self):
+        return self.__seconds
+
+    @seconds.setter
+    def seconds(self, value):
+        if not 0 <= value <= 59:
+            raise ValueError("Seconds must be between 0 and 59")
+        self.__seconds = value
