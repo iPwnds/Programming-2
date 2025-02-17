d3932598f29fc26611c96ce9972b8b45d357be8c
Florian Braun <Braunf25@proton.me>
2025-02-17 14:09:51 +0100

    completing part 07 (abstract classes)

--- 01-oo/07-abstract-classes/assignments/02-abstract-methods/student.py
+++ 01-oo/07-abstract-classes/assignments/02-abstract-methods/student.py
@@ -1,1 +1,64 @@
-# Write your code here
+from abc import ABC, abstractmethod
+
+
+class A(ABC):
+    def a(self):
+        self.b()
+
+    def e(self):
+        self.c()
+
+    @abstractmethod
+    def b(self):
+        pass
+
+    @abstractmethod
+    def c(self):
+        pass
+
+
+class B(A):
+    def b(self):
+        self.a()
+
+    def c(self):
+        self.e()
+
+
+class C(B):
+    def f(self):
+        pass
+
+
+class D(A, ABC):
+    def b(self):
+        self.f()
+
+    @abstractmethod
+    def f(self):
+        pass
+
+
+class E(D):
+    def c(self):
+        self.a()
+
+    def f(self):
+        self.e()
+
+    def g(self):
+        self.f()
+
+
+class F(ABC):
+    def a(self):
+        self.b()
+        self.f()
+
+    @abstractmethod
+    def b(self):
+        pass
+
+    @abstractmethod
+    def f(self):
+        pass
