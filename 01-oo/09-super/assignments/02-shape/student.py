d20598a988e69894aa07f51b630ddc0d0d5139f4
Florian Braun <Braunf25@proton.me>
2025-02-21 18:38:18 +0100

    Final chapter (09-super) for OO (Object Oriented)

--- 01-oo/09-super/assignments/02-shape/student.py
+++ 01-oo/09-super/assignments/02-shape/student.py
@@ -6,9 +6,71 @@ class Shape(ABC):
     @property
     @abstractmethod
     def area(self):
-        ...
+        pass
 
     @property
     @abstractmethod
     def perimeter(self):
-        ...
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def width(self):
+        return self._width
+
+    @property
+    def area(self):
+        return self.length * self.width
+
+    @property
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    @property
+    def side(self):
+        return self.length
+
+class Ellipse(Shape):
+    def __init__(self, major_radius, minor_radius):
+        self._major_radius = major_radius
+        self._minor_radius = minor_radius
+
+    @property
+    def major_radius(self):
+        return self._major_radius
+
+    @property
+    def minor_radius(self):
+        return self._minor_radius
+
+    @property
+    def area(self):
+        return self.major_radius * self.minor_radius * pi
+
+    @property
+    def perimeter(self):
+        raise NotImplementedError("Perimeter formula for an ellipse is complex and not implemented.")
+
+class Circle(Ellipse):
+    def __init__(self, radius):
+        super().__init__(radius, radius)
+
+    @property
+    def radius(self):
+        return self.major_radius
+
+    @property
+    def perimeter(self):
+        return 2 * self.radius * pi
