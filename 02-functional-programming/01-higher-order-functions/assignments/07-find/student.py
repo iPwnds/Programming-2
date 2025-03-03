50a8831e272390614e915fdb33e325b1ef338bb8
Florian Braun <Braunf25@proton.me>
2025-03-03 14:30:02 +0100

    02 -> 01 -> 09 Complete

--- 02-functional-programming/01-higher-order-functions/assignments/07-find/student.py
+++ 02-functional-programming/01-higher-order-functions/assignments/07-find/student.py
@@ -3,3 +3,12 @@ def find(items, condition):
         if condition(item):
             return item
     return None
+
+def has_consecutive_characters(string):
+    for index in range(len(string) - 1):
+        if string[index] == string[index + 1]:
+            return True
+    return False
+
+def find_string_with_consecutive_characters(strings):
+    return find(strings, has_consecutive_characters)
