5eb11505476a4d3cb980ab8969b9772565f7f0a9
Florian Braun <Braunf25@proton.me>
2025-03-10 17:28:01 +0100

    pt2 on windows

---
+++ 02-functional-programming/03-lambdas/assignments/04-sorting/student.py
@@ -0,0 +1,19 @@
+def sort_by_age(people):
+    # orders the Persons by increasing age.
+    return True
+
+def sort_by_decreasing_age(people):
+    # orders the Persons by decreasing age.
+    return True
+
+def sort_by_name(people):
+    # orders the Persons by name, alphabetically.
+    return True
+
+def sort_by_name_then_age(people):
+    # orders the Persons by name, and in case of equal names, by increasing age
+    return True
+
+def sort_by_name_then_decreasing_age(people):
+    # orders the Persons by name, and in case of equal names, by decreasing age
+    return True
