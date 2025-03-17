9b2707071a14d9b6a1c4ac06345db2c5b8ed2beb
Florian Braun <Braunf25@proton.me>
2025-03-17 13:26:13 +0100

    lambdas done

--- 02-functional-programming/03-lambdas/assignments/04-sorting/student.py
+++ 02-functional-programming/03-lambdas/assignments/04-sorting/student.py
@@ -1,4 +1,5 @@
 def sort_by_age(people):
+<<<<<<< HEAD
     return sorted(people, key=lambda person: person.age)
 
 def sort_by_decreasing_age(people):
@@ -12,3 +13,23 @@ def sort_by_name_then_age(people):
 
 def sort_by_name_then_decreasing_age(people):
     return sorted(people, key=lambda person: (person.name, -person.age))
+=======
+    # orders the Persons by increasing age.
+    return sorted(people, key=lambda people: people.age)
+
+def sort_by_decreasing_age(people):
+    # orders the Persons by decreasing age.
+    return sorted(people, key=lambda people : -people.age)
+
+def sort_by_name(people):
+    # orders the Persons by name, alphabetically.
+    return sorted(people, key=lambda people : people.name)
+
+def sort_by_name_then_age(people):
+    # orders the Persons by name, and in case of equal names, by increasing age
+    return sorted(people, key=lambda people : (people.name, people.age))
+
+def sort_by_name_then_decreasing_age(people):
+    # orders the Persons by name, and in case of equal names, by decreasing age
+    return sorted(people, key=lambda people : (people.name, -people.age))
+>>>>>>> 4c5538f (lambdas done)
--- 02-functional-programming/03-lambdas/assignments/06-min-max/student.py
+++ 02-functional-programming/03-lambdas/assignments/06-min-max/student.py
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
 def closest(points, target_points):
     return min(points, key=lambda point: ((point[0] - target_points[0]) ** 2) + ((point[1] - target_points[1]) ** 2))
+=======
+def closest(points, target_point):
+    
+    def distance(p1, p2):
+        return ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)**0.5
+
+    closest_point = points[0]
+    min_distance = distance(closest_point, target_point)
+
+    for point in points[1:]:
+        dist = distance(point, target_point)
+        if dist < min_distance:
+            closest_point = point
+            min_distance = dist
+
+    return closest_point
+>>>>>>> 4c5538f (lambdas done)
